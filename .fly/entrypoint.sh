62f1f76d72b651c5161335256322bf7e36ee68e3
LOC.ND <duchongkhoan100@gmail.com>
2025-06-14 11:50:00 +0700

    thử lại deploy

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -9,7 +9,7 @@ chown -R www-data:www-data /var/www/html
 
 if [ $# -gt 0 ]; then
     # If we passed a command, run it as root
-    exec "$@"
+    exec "$@"Add commentMore actions
 else
-    exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
+    exec supervisord -c /etc/supervisor/supervisord.conf
 fi
