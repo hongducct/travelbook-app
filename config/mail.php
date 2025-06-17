e91d3d86dee9b3b9e0db387ed7a9ca4a350b6fa4
hongducct <duchongkhoan100@gmail.com>
2025-06-17 08:34:54 +0700

    sửa lại default mail

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'travelbooking@hongducct.id.vn'),
+        'name' => env('MAIL_FROM_NAME', 'Travel Booking'),
     ],
 
     /*
